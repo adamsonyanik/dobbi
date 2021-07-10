a962e0191cf8a01f59077e830ab954a3446f05eb
Yanik Adamson <yadamsond@gmail.com>
2021-07-10 17:26:24 +0200

    init spring

---
+++ src/main/java/de/adamson/dobbi/DobbiApplication.java
@@ -0,0 +1,13 @@
+package de.adamson.dobbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobbiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/adamson/dobbi/DobbiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adamson.dobbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DobbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
